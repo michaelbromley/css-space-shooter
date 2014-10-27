7c881267e5113060cd498e1cb1e6184b9b51e461
Michael Bromley <michael@michaelbromley.co.uk>
2014-10-27 21:33:08 +0100

    Add animation control to track

--- scripts/track.js
+++ scripts/track.js
@@ -1,9 +1,20 @@
 function Track(el) {
-    this.container = el;
+    var self = this;
+    self.container = el;
+
+    [].forEach.call(self.container.querySelectorAll('.track'), function(track) {
+        track.style.webkitAnimationPlayState = 'paused';
+    });
+
+    this.show = function() {
+        [].forEach.call(self.container.querySelectorAll('.track'), function(track) {
+            track.style.webkitAnimationPlayState = 'running';
+        });
+    };
 
     this.update = function(ship) {
         var x = ship.x * -0.3;
         var y = ship.y * -0.3;
-        this.container.style.transform = "translateX(" + x + 'px) translateY(' + y + 'px)';
+        self.container.style.transform = "translateX(" + x + 'px) translateY(' + y + 'px)';
     }
 }
